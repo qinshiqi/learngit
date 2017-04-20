cc31dd3f6729fc0551e935c6d8ccc47e13bc4f2f
qinshiqi <qinshiqi@126.com>
2017-04-20 15:52:20 +0800

    git add testNewGit

---
+++ testNewGit/testNewGit/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  testNewGit
+//
+//  Created by qin carrey on 2017/3/23.
+//  Copyright © 2017年 qin carrey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //dfadsf
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
